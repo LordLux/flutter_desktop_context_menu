3a9d7aaa06706e9552b31d2e12027b99af4adaf5
LordLux <74434026+LordLux@users.noreply.github.com>
2025-06-05 00:59:46 +0200

    Refactor dark mode initialization and improve code formatting for readability

--- windows/flutter_desktop_context_menu_plugin.cpp
+++ windows/flutter_desktop_context_menu_plugin.cpp
@@ -44,15 +44,18 @@ void InitDarkMode() {
   if (hUxtheme) {
     // Ordinal 135 is SetPreferredAppMode in Win10 1903+
     SetPreferredAppMode = reinterpret_cast<PFN_SET_PREFERRED_APP_MODE>(
-        GetProcAddress(hUxtheme, MAKEINTRESOURCEA(135)));
+      GetProcAddress(hUxtheme, MAKEINTRESOURCEA(135))
+    );
 
     // Ordinal 133 is AllowDarkModeForWindow
     AllowDarkModeForWindow = reinterpret_cast<PFN_ALLOW_DARK_MODE_FOR_WINDOW>(
-        GetProcAddress(hUxtheme, MAKEINTRESOURCEA(133)));
+      GetProcAddress(hUxtheme, MAKEINTRESOURCEA(133))
+    );
 
     // Ordinal 132 is ShouldAppsUseDarkMode
     ShouldAppsUseDarkMode = reinterpret_cast<PFN_SHOULD_APPS_USE_DARK_MODE>(
-        GetProcAddress(hUxtheme, MAKEINTRESOURCEA(132)));
+      GetProcAddress(hUxtheme, MAKEINTRESOURCEA(132))
+    );
 
     // Force dark mode for the application
     if (SetPreferredAppMode) SetPreferredAppMode(PreferredAppModeForceDark);
@@ -73,6 +76,7 @@ void EnableDarkModeForContextMenu(HWND hwnd) {
 }
 
 namespace {
+
 const EncodableValue* ValueOrNull(const EncodableMap& map, const char* key) {
   auto it = map.find(EncodableValue(key));
   if (it == map.end()) {
@@ -80,82 +84,89 @@ const EncodableValue* ValueOrNull(const EncodableMap& map, const char* key) {
   }
   return &(it->second);
 }
-std::unique_ptr<flutter::MethodChannel<EncodableValue>,
-                std::default_delete<flutter::MethodChannel<EncodableValue>>>
-    channel = nullptr;
+
+std::unique_ptr<flutter::MethodChannel<EncodableValue>, std::default_delete<flutter::MethodChannel<EncodableValue>>> channel = nullptr;
+
 class FlutterDesktopContextMenuPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
+  
+  public:
+    static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
 
-  FlutterDesktopContextMenuPlugin(flutter::PluginRegistrarWindows* registrar);
+    FlutterDesktopContextMenuPlugin(flutter::PluginRegistrarWindows* registrar);
 
-  virtual ~FlutterDesktopContextMenuPlugin();
+    virtual ~FlutterDesktopContextMenuPlugin();
 
- private:
-  std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> g_converter;
+  private:
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> g_converter;
 
-  flutter::PluginRegistrarWindows* registrar;
+    flutter::PluginRegistrarWindows* registrar;
 
-  // The ID of the WindowProc delegate registration.
-  int window_proc_id = -1;
+    // The ID of the WindowProc delegate registration.
+    int window_proc_id = -1;
 
-  HMENU hMenu;
+    HMENU hMenu;
 
-  void FlutterDesktopContextMenuPlugin::_CreateMenu(HMENU menu,
-                                         UINT_PTR menuId,
-                                         EncodableMap args);
+    void FlutterDesktopContextMenuPlugin::_CreateMenu(
+      HMENU menu,
+      UINT_PTR menuId,
+      EncodableMap args
+    );
 
-  // Called for top-level WindowProc delegation.
-  std::optional<LRESULT> FlutterDesktopContextMenuPlugin::HandleWindowProc(HWND hwnd,
-                                                                UINT message,
-                                                                WPARAM wparam,
-                                                                LPARAM lparam);
-  HWND FlutterDesktopContextMenuPlugin::GetMainWindow();
-  void FlutterDesktopContextMenuPlugin::PopUp(
+    // Called for top-level WindowProc delegation.
+    std::optional<LRESULT> FlutterDesktopContextMenuPlugin::HandleWindowProc(
+      HWND hwnd,
+      UINT message,
+      WPARAM wparam,
+      LPARAM lparam
+    );
+    
+    HWND FlutterDesktopContextMenuPlugin::GetMainWindow();
+    void FlutterDesktopContextMenuPlugin::PopUp(
       const flutter::MethodCall<EncodableValue>& method_call,
-      std::unique_ptr<flutter::MethodResult<EncodableValue>> result);
+      std::unique_ptr<flutter::MethodResult<EncodableValue>> result
+    );
 
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
+    // Called when a method is called on this plugin's channel from Dart.
+    void HandleMethodCall(
       const flutter::MethodCall<EncodableValue>& method_call,
-      std::unique_ptr<flutter::MethodResult<EncodableValue>> result);
+      std::unique_ptr<flutter::MethodResult<EncodableValue>> result
+    );
 };
 
-// static
-void FlutterDesktopContextMenuPlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows* registrar) {
+// static:
+
+void FlutterDesktopContextMenuPlugin::RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar) {
   channel = std::make_unique<flutter::MethodChannel<EncodableValue>>(
-      registrar->messenger(), "flutter_desktop_context_menu",
-      &flutter::StandardMethodCodec::GetInstance());
+    registrar->messenger(),
+    "flutter_desktop_context_menu",
+    &flutter::StandardMethodCodec::GetInstance()
+  );
 
   auto plugin = std::make_unique<FlutterDesktopContextMenuPlugin>(registrar);
 
   channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto& call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
+    [plugin_pointer = plugin.get()](const auto& call, auto result) {
+      plugin_pointer->HandleMethodCall(call, std::move(result));
+    }
+  );
 
   registrar->AddPlugin(std::move(plugin));
 }
 
-FlutterDesktopContextMenuPlugin::FlutterDesktopContextMenuPlugin(
-    flutter::PluginRegistrarWindows* registrar)
-    : registrar(registrar) {
+FlutterDesktopContextMenuPlugin::FlutterDesktopContextMenuPlugin(flutter::PluginRegistrarWindows* registrar) : registrar(registrar) {
   InitDarkMode();
   
   window_proc_id = registrar->RegisterTopLevelWindowProcDelegate(
-      [this](HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
-        return HandleWindowProc(hwnd, message, wparam, lparam);
-      });
+    [this](HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
+      return HandleWindowProc(hwnd, message, wparam, lparam);
+    }
+  );
 }
 
 FlutterDesktopContextMenuPlugin::~FlutterDesktopContextMenuPlugin() {}
 
-void FlutterDesktopContextMenuPlugin::_CreateMenu(HMENU menu,
-                                       UINT_PTR menuId,
-                                       EncodableMap args) {
-  EncodableList items =
-      std::get<EncodableList>(args.at(EncodableValue("items")));
+void FlutterDesktopContextMenuPlugin::_CreateMenu(HMENU menu, UINT_PTR menuId, EncodableMap args) {
+  EncodableList items = std::get<EncodableList>(args.at(EncodableValue("items")));
 
   // Store the menuId in the application defined `dwMenuData` field. This
   // allows us to retain the dart defined item id for submenu items.
@@ -174,22 +185,19 @@ void FlutterDesktopContextMenuPlugin::_CreateMenu(HMENU menu,
   for (EncodableValue item_value : items) {
     EncodableMap item_map = std::get<EncodableMap>(item_value);
     int id = std::get<int>(item_map.at(EncodableValue("id")));
-    std::string type =
-        std::get<std::string>(item_map.at(EncodableValue("type")));
-    std::string label =
-        std::get<std::string>(item_map.at(EncodableValue("label")));
+    std::string type = std::get<std::string>(item_map.at(EncodableValue("type")));
+    std::string label = std::get<std::string>(item_map.at(EncodableValue("label")));
     auto* checked = std::get_if<bool>(ValueOrNull(item_map, "checked"));
     bool disabled = std::get<bool>(item_map.at(EncodableValue("disabled")));
 
     UINT_PTR item_id = id;
     UINT uFlags = MF_STRING;
 
-    if (disabled) {
-      uFlags |= MF_GRAYED;
-    }
+    if (disabled) uFlags |= MF_GRAYED;
 
     if (type.compare("separator") == 0) {
       AppendMenuW(menu, MF_SEPARATOR, item_id, NULL);
+      
     } else {
       if (type.compare("checkbox") == 0) {
         if (checked == nullptr) {
@@ -197,12 +205,17 @@ void FlutterDesktopContextMenuPlugin::_CreateMenu(HMENU menu,
         } else {
           uFlags |= (*checked == true ? MF_CHECKED : MF_UNCHECKED);
         }
+        
       } else if (type.compare("submenu") == 0) {
         uFlags |= MF_POPUP;
         HMENU sub_menu = ::CreatePopupMenu();
+        
         _CreateMenu(
-            sub_menu, item_id,
-            std::get<EncodableMap>(item_map.at(EncodableValue("submenu"))));
+          sub_menu,
+          item_id,
+          std::get<EncodableMap>(item_map.at(EncodableValue("submenu")))
+        );
+        
         item_id = reinterpret_cast<UINT_PTR>(sub_menu);
       }
       AppendMenuW(menu, uFlags, item_id, g_converter.from_bytes(label).c_str());
@@ -210,18 +223,19 @@ void FlutterDesktopContextMenuPlugin::_CreateMenu(HMENU menu,
   }
 }
 
-std::optional<LRESULT> FlutterDesktopContextMenuPlugin::HandleWindowProc(HWND hWnd,
-                                                              UINT message,
-                                                              WPARAM wParam,
-                                                              LPARAM lParam) {
+std::optional<LRESULT> FlutterDesktopContextMenuPlugin::HandleWindowProc(
+  HWND hWnd,
+  UINT message,
+  WPARAM wParam,
+  LPARAM lParam
+) {
   std::optional<LRESULT> result;
   if (message == WM_COMMAND) {
     flutter::EncodableMap eventData = flutter::EncodableMap();
-    eventData[flutter::EncodableValue("id")] =
-        flutter::EncodableValue((int)LOWORD(wParam));
+    eventData[flutter::EncodableValue("id")] = flutter::EncodableValue((int)LOWORD(wParam));
 
-    channel->InvokeMethod("onMenuItemClick",
-                          std::make_unique<flutter::EncodableValue>(eventData));
+    channel->InvokeMethod("onMenuItemClick", std::make_unique<flutter::EncodableValue>(eventData));
+    
   } else if (message == WM_MENUSELECT) {
     auto itemParam = LOWORD(wParam);
     flutter::EncodableMap eventData = flutter::EncodableMap();
@@ -233,19 +247,15 @@ std::optional<LRESULT> FlutterDesktopContextMenuPlugin::HandleWindowProc(HWND hW
       menuInfo.cbSize = sizeof(MENUINFO);
       menuInfo.fMask = MIM_MENUDATA;
       auto getSubMenuInfoSuccess = GetMenuInfo(subMenuHandle, &menuInfo);
-      if (!getSubMenuInfoSuccess) {
-        return std::nullopt;
-      }
+      
+      if (!getSubMenuInfoSuccess) return std::nullopt;
 
-      eventData[flutter::EncodableValue("id")] =
-          flutter::EncodableValue((int)menuInfo.dwMenuData);
+      eventData[flutter::EncodableValue("id")] = flutter::EncodableValue((int)menuInfo.dwMenuData);
     } else {
-      eventData[flutter::EncodableValue("id")] =
-          flutter::EncodableValue((int)itemParam);
+      eventData[flutter::EncodableValue("id")] = flutter::EncodableValue((int)itemParam);
     }
 
-    channel->InvokeMethod("onMenuItemHighlight",
-                          std::make_unique<flutter::EncodableValue>(eventData));
+    channel->InvokeMethod("onMenuItemHighlight", std::make_unique<flutter::EncodableValue>(eventData));
   }
 
   return std::nullopt;
@@ -256,21 +266,23 @@ HWND FlutterDesktopContextMenuPlugin::GetMainWindow() {
 }
 
 void FlutterDesktopContextMenuPlugin::PopUp(
-    const flutter::MethodCall<EncodableValue>& method_call,
-    std::unique_ptr<flutter::MethodResult<EncodableValue>> result) {
+  const flutter::MethodCall<EncodableValue>& method_call,
+  std::unique_ptr<flutter::MethodResult<EncodableValue>> result
+) {
   HWND hWnd = GetMainWindow();
   EnableDarkModeForContextMenu(hWnd);
 
   const EncodableMap& args = std::get<EncodableMap>(*method_call.arguments());
 
-  std::string placement =
-      std::get<std::string>(args.at(EncodableValue("placement")));
-  double device_pixel_ratio =
-      std::get<double>(args.at(flutter::EncodableValue("devicePixelRatio")));
+  std::string placement = std::get<std::string>(args.at(EncodableValue("placement")));
+  double device_pixel_ratio = std::get<double>(args.at(flutter::EncodableValue("devicePixelRatio")));
 
   hMenu = CreatePopupMenu();
-  _CreateMenu(hMenu, 0,
-              std::get<EncodableMap>(args.at(EncodableValue("menu"))));
+  _CreateMenu(
+    hMenu,
+    0,
+    std::get<EncodableMap>(args.at(EncodableValue("menu")))
+  );
 
   double x, y;
 
@@ -282,13 +294,10 @@ void FlutterDesktopContextMenuPlugin::PopUp(
   y = cursorPos.y;
 
   if (args.find(EncodableValue("position")) != args.end()) {
-    const EncodableMap& position =
-        std::get<EncodableMap>(args.at(EncodableValue("position")));
+    const EncodableMap& position = std::get<EncodableMap>(args.at(EncodableValue("position")));
 
-    double position_x =
-        std::get<double>(position.at(flutter::EncodableValue("x")));
-    double position_y =
-        std::get<double>(position.at(flutter::EncodableValue("y")));
+    double position_x = std::get<double>(position.at(flutter::EncodableValue("x")));
+    double position_y = std::get<double>(position.at(flutter::EncodableValue("y")));
 
     RECT window_rect, client_rect;
     TITLEBARINFOEX title_bar_info;
@@ -297,18 +306,15 @@ void FlutterDesktopContextMenuPlugin::PopUp(
     GetClientRect(hWnd, &client_rect);
     title_bar_info.cbSize = sizeof(TITLEBARINFOEX);
     ::SendMessage(hWnd, WM_GETTITLEBARINFOEX, 0, (LPARAM)&title_bar_info);
-    int32_t title_bar_height =
-        title_bar_info.rcTitleBar.bottom == 0
-            ? 0
-            : title_bar_info.rcTitleBar.bottom - title_bar_info.rcTitleBar.top;
+    int32_t title_bar_height = 
+      title_bar_info.rcTitleBar.bottom == 0
+        ? 0
+        : title_bar_info.rcTitleBar.bottom - title_bar_info.rcTitleBar.top;
 
-    int border_thickness =
-        ((window_rect.right - window_rect.left) - client_rect.right) / 2;
+    int border_thickness = ((window_rect.right - window_rect.left) - client_rect.right) / 2;
 
-    x = static_cast<double>((position_x * device_pixel_ratio) +
-                            (window_rect.left + border_thickness));
-    y = static_cast<double>((position_y * device_pixel_ratio) +
-                            (window_rect.top + title_bar_height));
+    x = static_cast<double>((position_x * device_pixel_ratio) + (window_rect.left + border_thickness));
+    y = static_cast<double>((position_y * device_pixel_ratio) + (window_rect.top + title_bar_height));
   }
 
   if (placement.compare("topLeft") == 0) {
@@ -321,15 +327,23 @@ void FlutterDesktopContextMenuPlugin::PopUp(
     uFlags = TPM_TOPALIGN | TPM_LEFTALIGN;
   }
 
-  TrackPopupMenu(hMenu, uFlags, static_cast<int>(x), static_cast<int>(y), 0,
-                 hWnd, NULL);
+  TrackPopupMenu(
+    hMenu,
+    uFlags,
+    static_cast<int>(x),
+    static_cast<int>(y),
+    0,
+    hWnd,
+    NULL
+  );
 
   result->Success(EncodableValue(true));
 }
 
 void FlutterDesktopContextMenuPlugin::HandleMethodCall(
-    const flutter::MethodCall<EncodableValue>& method_call,
-    std::unique_ptr<flutter::MethodResult<EncodableValue>> result) {
+  const flutter::MethodCall<EncodableValue>& method_call,
+  std::unique_ptr<flutter::MethodResult<EncodableValue>> result
+) {
   if (method_call.method_name().compare("popUp") == 0) {
     PopUp(method_call, std::move(result));
   } else {
@@ -339,9 +353,8 @@ void FlutterDesktopContextMenuPlugin::HandleMethodCall(
 
 }  // namespace
 
-void FlutterDesktopContextMenuPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
+void FlutterDesktopContextMenuPluginRegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar) {
   FlutterDesktopContextMenuPlugin::RegisterWithRegistrar(
-      flutter::PluginRegistrarManager::GetInstance()
-          ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar));
+    flutter::PluginRegistrarManager::GetInstance() ->GetRegistrar<flutter::PluginRegistrarWindows>(registrar)
+  );
 }
